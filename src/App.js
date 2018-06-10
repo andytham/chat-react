c9b9a1a1df63519d1d9b60f8ea3332c50038f7cb
andy <thamandyy@gmail.com>
2018-06-10 19:07:43 -0400

    'cleanup'

--- src/App.js
+++ src/App.js
@@ -26,105 +26,19 @@ class App extends React.Component {
       username: "",
       loginSuccess: false
     }
-    // this.renderChat = this.renderChat.bind(this)
-    this.panel = React.createRef();
-    this.onJoin = this.onJoin.bind(this)
-    this.getHistory = this.getHistory.bind(this)
-    this.updateChat = this.updateChat.bind(this)
-    this.onSendMessage = this.onSendMessage.bind(this)
+
     this.onSuccess = this.onSuccess.bind(this)
   }
-  componentDidMount(){
-    this.onJoin();
-    lock.on("authenticated", function(authResult) {
-      // Use the token in authResult to getUserInfo() and save it to localStorage
-      lock.getUserInfo(authResult.accessToken, function(error, profile) {
-        if (error) {
-          // Handle error
-          return;
-        }
-    
-        // document.getElementById('nick').textContent = profile.nickname;
-    
-        localStorage.setItem('accessToken', authResult.accessToken);
-        localStorage.setItem('profile', JSON.stringify(profile));
-      });
-    });
-  }
-
-  // goTo(route) {
-  //   this.props.history.replace(`/${route}`)
-  // }
-
-  // login() {
-  //   this.props.auth.login();
-  // }
-
-  // logout() {
-  //   this.props.auth.logout();
-  // }
-
-
-  getHistory(){
-    this.state.client.history(this.updateChat)
-  }
-
-  updateChat(entry){
-    this.setState({
-      chatHistory: entry
-    })
-  }
-
-  componentDidUpdate(){
-  }
-  onJoin(){
-    this.state.client.join();
-    this.getHistory();
-  }
-
-  onSendMessage(msg, cb){
-    this.state.client.message(msg, cb)
-    this.state.client.receive(this.updateChat)
-  }
+  componentDidMount(){}
 
   onSuccess(username){
-    console.log("sucess");
     this.setState({
       username: username,
       loginSuccess: true
-    }, () => {
-      console.log(this.state.username, 'this is state app');
-      
-      this.props.history.push({
-        pathname: "/chat",
-        state: { username: this.state.username }
-      });
     })
-    // console.log(this.state.username, 'appjs username');
   }
 
   render(){
-    // auth.login();
-    // lock.show();
-    self = this;
-    lock.on("authenticated", function(authResult) {
-			// Use the token in authResult to getUserInfo() and save it to localStorage
-			lock.getUserInfo(authResult.accessToken, function(error, profile) {
-				if (error) {
-					// Handle error
-					return;
-        }
-        
-        console.log('authenticated as: ', profile.nickname)
-        console.log(authResult.accessToken);
-        self.onSuccess(profile.nickname)
-        
-				// document.getElementById('nick').textContent = profile.nickname;
-		
-				localStorage.setItem('accessToken', authResult.accessToken);
-				localStorage.setItem('profile', JSON.stringify(profile));
-			});
-		});
     const ChatroomComponent = () => {
       <Chatroom
         chatHistory={this.state.chatHistory}
@@ -132,18 +46,13 @@ class App extends React.Component {
           this.onSendMessage}
       />
     }
-    // console.log("app js is rendering");
-    // console.log("APP STATE", this.state);
+
     return(
         <div id="auth" className="App">
           <Switch>
             <Route exact path="/" render={(props) => <Home {...this.props} loginSuccess={this.state.loginSuccess} />} />
             <Route exact path="/login" render={() => <AuthComponent onSuccess={this.onSuccess}/>} />
             <Route exact path="/chat" render={() =>  <Chatroom
-              chatHistory={this.state.chatHistory}
-              onSendMessage={
-                this.onSendMessage}
-              username={this.state.username}
             />} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/profile" render={() => <Profile username={this.state.username}/>} />
