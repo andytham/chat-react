21733b7c2389343d7c995930db2c583e2d9e3970
andy <thamandyy@gmail.com>
2018-06-10 19:40:31 -0400

    aws test

--- src/auth-config.js
+++ src/auth-config.js
@@ -2,7 +2,7 @@ import Auth0Lock from 'auth0-lock';
 var lock = new Auth0Lock('v0MVweBrzRBmP74attU15PyCFM3HJQvY', 'andytham.auth0.com', {
   container: 'auth',
   auth: {
-    redirectUrl: 'http://localhost:3000/chat',    // If not specified, defaults to the current page 
+    redirectUrl: 'http://ec2-18-188-70-45.us-east-2.compute.amazonaws.com:3000/chat',    // If not specified, defaults to the current page 
     configurationBaseUrl: 'https://cdn.auth0.com',
     responseType: 'token',
     params: {
