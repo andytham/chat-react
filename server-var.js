3054c5f4d0fab222a30f5d52d205e72ed7c37d7d
andy <thamandyy@gmail.com>
2018-06-13 04:43:35 -0400

    front deploy, back still local

--- server-var.js
+++ server-var.js
@@ -1,7 +1,8 @@
 let url = {
 	PROFILES_API: "http://localhost:8081/user-profiles",
 	CHAT_HISTORY_API: "http://localhost:8082/history",
-	FRONT_END: "http://localhost:3000"
+	// FRONT_END: "http://localhost:3000"
+	FRONT_END: "http://ec2-18-219-41-84.us-east-2.compute.amazonaws.com:3000"
 }
 
 module.exports = url;
