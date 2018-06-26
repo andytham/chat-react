045ceb831d7cfb87f9707ebb17c5045e2e30797b
andy <thamandyy@gmail.com>
2018-06-26 01:54:33 -0400

    deploy

--- server-var.js
+++ server-var.js
@@ -2,14 +2,17 @@ let url = {
 	// PROFILES_API: "http://localhost:8081/user-profiles",
 	// CHAT_HISTORY_API: "http://localhost:8082/history",
 
+	// PROFILES_API: "http://ec2-18-218-63-92.us-east-2.compute.amazonaws.com:8080/user-profiles-api/user-profiles",
+	// CHAT_HISTORY_API: "http://ec2-18-218-63-92.us-east-2.compute.amazonaws.com:8080/chat-history/history",
+	// PROFILES_API: "http://localhost:8080/api/statuses",
+	// CHAT_HISTORY_API: "http://localhost:8080/api/history",
+	PROFILES_API: "http://express.andytham.com/api/statuses",
+	CHAT_HISTORY_API: "http://express.andytham.com/api/history",
 
-	PROFILES_API: "http://ec2-18-218-63-92.us-east-2.compute.amazonaws.com:8080/user-profiles-api/user-profiles",
-	CHAT_HISTORY_API: "http://ec2-18-218-63-92.us-east-2.compute.amazonaws.com:8080/chat-history/history",
 
-
-	FRONT_END: "http://localhost:3000",
+	// FRONT_END: "http://localhost:3000",
 	// FRONT_END: "http://ec2-18-219-41-84.us-east-2.compute.amazonaws.com:3000",
-	// FRONT_END: "http://chat.andytham.com",
+	FRONT_END: "http://chat.andytham.com",
 
 }
 
